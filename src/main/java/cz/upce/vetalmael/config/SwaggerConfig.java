b20d88b6cb03095159f05bb58f6ea62bc803ab27
vanap <st52572@student.upce.cz>
2020-10-19 19:42:14 +0200

    making SWAGGER_AUTH_KEY final

--- src/main/java/cz/upce/vetalmael/config/SwaggerConfig.java
+++ src/main/java/cz/upce/vetalmael/config/SwaggerConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-    public static String SWAGGER_AUTH_KEY = "bearerAuth";
+    public final static String SWAGGER_AUTH_KEY = "bearerAuth";
 
     @Bean
     OpenAPI customOpenAPI() {
