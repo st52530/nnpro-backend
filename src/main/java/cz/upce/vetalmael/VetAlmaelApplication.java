148174bdcd47af86537098dfc4fe119801d60d47
maksim-khiuttiulia <43935623+maksim-khiuttiulia@users.noreply.github.com>
2020-10-24 23:28:22 +0200

    delete cors setting

--- src/main/java/cz/upce/vetalmael/VetAlmaelApplication.java
+++ src/main/java/cz/upce/vetalmael/VetAlmaelApplication.java
@@ -9,16 +9,5 @@ public class VetAlmaelApplication {
     public static void main(String[] args) {
         SpringApplication.run(VetAlmaelApplication.class, args);
     }
-    
-    
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**").allowedMethods("GET", "POST", "PUT", "DELETE");
-            }
-        };
-    }
-
+   
 }
