2770feaf591e8eecc3155326e0f440c25a8804d1
maksim-khiuttiulia <43935623+maksim-khiuttiulia@users.noreply.github.com>
2020-10-27 21:49:13 +0100

    Fix login endpoint

--- src/main/java/cz/upce/vetalmael/security/SecurityConstants.java
+++ src/main/java/cz/upce/vetalmael/security/SecurityConstants.java
@@ -7,6 +7,6 @@ public class SecurityConstants {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/client/sign-up";
-    public static final String SIGN_IN_URL = "/user/login";
+    public static final String SIGN_IN_URL = "/users/login";
 
 }
