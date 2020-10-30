4333b95f6acea0d051ba481ebf47b0db17de7a94
Petr Váňa <43939385+st52572@users.noreply.github.com>
2020-10-30 22:30:05 +0100

    sign-up

--- src/main/java/cz/upce/vetalmael/security/SecurityConstants.java
+++ src/main/java/cz/upce/vetalmael/security/SecurityConstants.java
@@ -6,7 +6,7 @@ public class SecurityConstants {
     public static final long EXPIRATION_TIME = 3600_000; // 15 mins
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/client/sign-up";
+    public static final String SIGN_UP_URL = "/clients/sign-up";
     public static final String SIGN_IN_URL = "/users/login";
 
 }
