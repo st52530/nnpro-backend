b5ec12e5dd0f3d90664fb770dc6bc470732b3ad9
vanap <st52572@student.upce.cz>
2020-11-03 00:01:37 +0100

    latest get updated

--- src/main/java/cz/upce/vetalmael/repository/ClinicConsumableRepository.java
+++ src/main/java/cz/upce/vetalmael/repository/ClinicConsumableRepository.java
@@ -3,5 +3,11 @@ package cz.upce.vetalmael.repository;
 import cz.upce.vetalmael.model.ClinicConsumable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClinicConsumableRepository extends JpaRepository<ClinicConsumable, Integer> {
+
+    ClinicConsumable findByClinic_IdClinicAndConsumable_IdConsumable(int idClinic, int idConsumable);
+
+    List<ClinicConsumable> findAllByClinic_IdClinic(int idClinic);
 }
