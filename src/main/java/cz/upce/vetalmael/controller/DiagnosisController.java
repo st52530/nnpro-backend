6d86c7bcd43cea0fa10031c08aceff51a978830f
vanap <st52572@student.upce.cz>
2020-11-08 17:38:24 +0100

    Excel import diagnosis

--- src/main/java/cz/upce/vetalmael/controller/DiagnosisController.java
+++ src/main/java/cz/upce/vetalmael/controller/DiagnosisController.java
@@ -1,5 +1,6 @@
 package cz.upce.vetalmael.controller;
 
+import cz.upce.vetalmael.model.Consumable;
 import cz.upce.vetalmael.model.Diagnosis;
 import cz.upce.vetalmael.model.dto.DiagnosisDto;
 import cz.upce.vetalmael.service.DiagnosisService;
@@ -8,8 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
+
+import java.io.IOException;
+import java.util.List;
 
 import static cz.upce.vetalmael.config.SwaggerConfig.SWAGGER_AUTH_KEY;
 
@@ -25,6 +30,15 @@ public class DiagnosisController {
     @PostMapping
     public ResponseEntity<Diagnosis> addAnimal(@RequestBody DiagnosisDto diagnosisDto) {
         return ResponseEntity.ok(diagnosisService.addDiagnosis(diagnosisDto));
+    }
+
+    @PostMapping(value = "/excel", consumes = {"multipart/form-data"})
+    public ResponseEntity<List<Diagnosis>> importDiagnoses(@RequestParam("file") MultipartFile file) {
+        try {
+            return ResponseEntity.ok(diagnosisService.importDiagnosis(file));
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @Transactional(rollbackOn = Exception.class)
