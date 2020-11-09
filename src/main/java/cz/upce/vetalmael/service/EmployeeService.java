9817303389a1bfd1898b1b6e843ec37dc3d9006f
vanap <st52572@student.upce.cz>
2020-11-09 22:04:38 +0100

    employees/clinic/{idClinic}

--- src/main/java/cz/upce/vetalmael/service/EmployeeService.java
+++ src/main/java/cz/upce/vetalmael/service/EmployeeService.java
@@ -3,9 +3,12 @@ package cz.upce.vetalmael.service;
 import cz.upce.vetalmael.model.User;
 import cz.upce.vetalmael.model.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
 
     User addEmployee(EmployeeDto employeeDto, int idClinic);
     User editEmployee(EmployeeDto employeeDto,int idEmployee, int idClinic);
     void removeEmployee(int idEmployee);
+    List<User> getClinicEmployees(int idClinic);
 }
