a9146a0a3284f99738cd87842a67e5ca04cf8930
vanap <st52572@student.upce.cz>
2020-11-21 14:26:33 +0100

    EXPIRATION_TIME of token set to week

--- src/main/java/cz/upce/vetalmael/security/SecurityConstants.java
+++ src/main/java/cz/upce/vetalmael/security/SecurityConstants.java
@@ -3,7 +3,7 @@ package cz.upce.vetalmael.security;
 public class SecurityConstants {
 
     public static final String SECRET = "SECRET_KEY";
-    public static final long EXPIRATION_TIME = 604800000; // 15 mins
+    public static final long EXPIRATION_TIME = 604800000; // week
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/clients/sign-up";
